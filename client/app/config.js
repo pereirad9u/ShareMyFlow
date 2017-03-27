5aef1b1912494a96737988b386fe626654a234a5
Guillaume Launay <g.launay96@gmail.com>
2017-03-27 11:04:37 +0200

    Mise à jour du dépot depuis nos dépots personnels

--- client/app/config.js
+++
@@ -1,2 +0,0 @@
-var scopes = ['playlist-modify-private','user-read-private','user-read-birthdate','user-read-email', 'user-library-read','user-follow-read', 'playlist-read-private'];
-Accounts.ui.config({'requestPermissions':{'spotify':scopes}});
